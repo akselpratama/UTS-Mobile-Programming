4e89d44c7752bde1f0c103db6b1003e2ec300120
akselpratama <aksel.pratama06@gmail.com>
2020-05-07 11:22:04 +0800

    login username pss nim

--- app/src/main/java/com/example/utsapp/MainActivity.java
+++ app/src/main/java/com/example/utsapp/MainActivity.java
@@ -37,11 +37,11 @@ public class MainActivity extends AppCompatActivity {
                 break;
         }
         if (!isValidEmail(email)) {
-            emailEditText.setError("Email Tidak Valid");
+            emailEditText.setError("Username Salah !");
         }
         final String pass = passEditText.getText().toString();
         if (!isValidPassword(pass)) {
-            passEditText.setError("Password tidak boleh kosong / lebih atau sama dengan 4 karakter");
+            passEditText.setError("Password Salah !");
         }
         if (isValidEmail(email) && isValidPassword(pass)) {
             Toast.makeText(getApplicationContext(), "Anda berhasil login ",Toast.LENGTH_LONG).show();
@@ -55,17 +55,16 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public boolean isValidPassword (String password) {
-        if (password != null && password.length() >= 4) {
+        if (password != null && password.equals("18101057")) {
             return true;
         }
         return false;
     }
-    public boolean isValidEmail (String email) {
-        String EMAIL_PATTERN = "^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@"
-                + "[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
 
-        Pattern pattern = Pattern.compile(EMAIL_PATTERN);
-        Matcher matcher = pattern.matcher(email);
-        return matcher.matches();
+    public boolean isValidEmail(String username) {
+        if (username != null && username.equals("18101057")) {
+            return true;
+        }
+        return false;
     }
 }
